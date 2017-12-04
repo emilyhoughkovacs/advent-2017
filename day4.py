3e5e6f1c31887021321ad9ede8d8ab76adc002be
Emily Hough-Kovacs <emilyhk@spotify.com>
2017-12-04 14:14:46 -0500

    day four star 2

--- day4.py
+++ day4.py
@@ -1,3 +1,27 @@
 with open("inputday4.txt", "r") as f:
   rows = [x for x in f]
 f.close()
+
+valid = 0
+
+def isValid(passphrase):
+  passphrase = passphrase.split()
+  newpp = []
+  for word in passphrase:
+    newpp.append(''.join(sorted(word)))
+  if len(newpp)==len(set(newpp)):
+    return True
+  else:
+    return False
+
+for row in rows:
+  if isValid(row):
+    valid += 1
+
+print valid
+
+# print isValid("abcde fghij")
+# print isValid("abcde xyz ecdab")
+# print isValid("a ab abc abd abf abj")
+# print isValid("iiii oiii ooii oooi oooo")
+# print isValid("oiii ioii iioi iiio")
