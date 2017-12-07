b4b1b4f77fe5fd9414c41ff3796c72feee2378a6
Emily Hough-Kovacs <emilyhk@spotify.com>
2017-12-07 16:18:03 -0500

    day 6 a day late

--- day6.py
+++ day6.py
@@ -4,25 +4,41 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 def get_input():
-  with open("inputday5.txt", "r") as f:
-    return file.readlines()
+  with open("inputday6.txt", "r") as f:
+    blah = [x.split() for x in f][0]
+    return [int(x) for x in blah]
 
-def worker(row, pt2=False):
-  # do work here
-  # return a true or false if possible
+def worker(banks, pt2=False):
+  most = max(banks)
+  i = banks.index(most)
+  dist = len(banks)-i
+  banks[i] = 0
+  j = i
+  for j in range(i+1, most+i+1):
+    banks[j % len(banks)] += 1
+  return banks
 
 def test():
-  assert worker("test1") is True
-  assert worker("test2") is True
-  assert worker("test3") is True
-  assert worker("test4") is True
-  assert worker("test5", pt2=True) is True
-  assert worker("test6", pt2=True) is True
-  assert worker("test7", pt2=True) is True
-  assert worker("test8", pt2=True) is True
+  # assert worker([0, 2, 7, 0]) == 6
+  assert worker([0, 2, 7, 0]) == [2, 4, 1, 2]
+  assert worker([3, 1, 2, 3]) == [0, 2, 3, 4]
+  assert worker([4, 1, 2, 3]) == [1, 2, 3, 4]
+  assert worker([3, 1, 2, 3]) == [0, 2, 3, 4]
+  assert worker([0, 2, 3, 4]) == [1, 3, 4, 1]
+  assert worker([1, 3, 4, 1]) == [2, 4, 1, 2]
+  # assert worker("test6", pt2=True) is True
+  # assert worker("test7", pt2=True) is True
+  # assert worker("test8", pt2=True) is True
+  return True
 
 def main():
   input = get_input()
+  # input = [0, 2, 7, 0]
+  seen = []
+  while input not in seen:
+    seen.append(input[:])
+    input = worker(input)
+  print len(seen)-seen.index(input)
 
   ## do work on each line of input here
   ## e.g. "how many rows of input are this"
