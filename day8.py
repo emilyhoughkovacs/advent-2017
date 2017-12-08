2927ce60594eeb50f5336768a8b39d164bc8156f
Emily Hough-Kovacs <emilyhk@spotify.com>
2017-12-08 14:00:29 -0500

    day 8 framework

---
+++ day8.py
@@ -0,0 +1,40 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def get_input():
+  with open("inputday7.txt", "r") as f:
+    return f.readlines()
+
+def splitLine(row):
+  row = row.split()
+  node = row[0]
+  if len(row) > 2 and row[2] == '->':
+    children = row[3:]
+  else:
+    children = []
+  return node, children
+
+# def worker(banks, pt2=False):
+
+def test():
+  # assert worker([0, 2, 7, 0]) == 6
+  # assert worker("test6", pt2=True) is True
+  # assert worker("test7", pt2=True) is True
+  # assert worker("test8", pt2=True) is True
+  return True
+
+def main():
+  input = get_input()
+  print splitLine(input[0])
+  # input = [0, 2, 7, 0]
+
+  ## do work on each line of input here
+  ## e.g. "how many rows of input are this"
+  ## valid = sum([1 for row in input if isValid(row)])
+  ## logger.info("Result: %s" % valid)
+
+if __name__ == "__main__":
+  if test():
+    main()
